f18b26c95cb9fd89260b2a7645d0e3fc7ad5feb4
yaasirf <148680817+yaasirf@users.noreply.github.com>
2024-05-13 20:01:23 +0700

    Update graph2.py

--- graph2.py
+++ graph2.py
@@ -1,1 +1,29 @@
-print ("tes")
+class Peta:
+    def __init__(self):
+        self.listKota = {}
+
+    def printPeta(self):
+        for kota in self.listKota:
+            print(kota, ":", self.listKota[kota])
+
+    def tambahkanKota(self, kota):
+        if kota not in self.listKota:
+            self.listKota[kota] = {}
+            return True
+        return False
+
+    def hapusKota(self, hapusKota):
+        if hapusKota in self.listKota:
+            for kotaLain in self.listKota:
+                if hapusKota in self.listKota[kotaLain]:
+                    del self.listKota[kotaLain][hapusKota]
+            del self.listKota[hapusKota]
+            return True
+        return False
+
+    def tambahkanJalan(self, kota1, kota2, jarak):
+        if kota1 in self.listKota and kota2 in self.listKota:
+            self.listKota[kota2][kota1] = jarak, 'KM'
+            self.listKota[kota1][kota2] = jarak, 'KM'
+            return True
+        return False
